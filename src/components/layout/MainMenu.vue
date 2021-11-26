1fce989a8659a55e0ac15343cdb871291b7ad2b0
Maja <pedic.maja@gmail.com>
2021-11-26 23:50:19 +0100

    Added navigation

---
+++ src/components/layout/MainMenu.vue
@@ -0,0 +1,14 @@
+<template>
+<div class="main-menu">
+  <router-link to="/reservations">Reservations</router-link>
+  <router-link to="/halls">Halls</router-link>
+  <router-link to="/tables">Tables</router-link>
+  <router-link to="/personnel">Personnel</router-link>
+</div>
+</template>
+
+<script>
+export default {
+  name: "MainMenu"
+}
+</script>
