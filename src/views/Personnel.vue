7b3759b512530868e2bd488915fab819979719da
Maja <pedic.maja@gmail.com>
2021-11-27 20:17:02 +0100

    Adds personnel layout

--- src/views/Personnel.vue
+++ src/views/Personnel.vue
@@ -1,5 +1,60 @@
 <template>
-<h1>Personnel</h1>
+  <h1>Personnel</h1>
+  <div class="container personnel">
+
+    <div class="card">
+
+      <div class="card-header">
+        <button class="button-rounded button-add-new">Add new</button>
+      </div>
+
+      <div class="items-list personnel-list">
+
+        <div class="items-list-row personnel-row">
+          <div class="personnel-name">John Doe
+            <div class="list-tag">Manager</div>
+          </div>
+          <div class="list-actions">
+            <button class="list-button">Edit</button>
+            <button class="list-button">Delete</button>
+          </div>
+        </div>
+
+<!--        <div class="items-list-row personnel-row">
+          <div class="personnel-name">John Doe
+            <div class="list-tag">Manager</div>
+          </div>
+          <div class="list-actions">
+            <button class="list-button">Edit</button>
+            <button class="list-button">Delete</button>
+          </div>
+        </div>-->
+
+        <div class="items-list-row personnel-row">
+          <div class="personnel-name">John Doe
+            <div class="list-tag">Manager</div>
+          </div>
+          <div class="list-actions">
+            <button class="list-button">Edit</button>
+            <button class="list-button">Delete</button>
+          </div>
+        </div>
+
+        <div class="items-list-row personnel-row">
+          <div class="personnel-name">John Doe
+            <div class="list-tag">Manager</div>
+          </div>
+          <div class="list-actions">
+            <button class="list-button">Edit</button>
+            <button class="list-button">Delete</button>
+          </div>
+        </div>
+
+      </div>
+
+    </div>
+  </div>
+
 </template>
 
 <script>
