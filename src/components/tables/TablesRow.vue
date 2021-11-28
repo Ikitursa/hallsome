9c710e444c26fa6011f0eaa433b372513e108fc1
Maja <pedic.maja@gmail.com>
2021-11-28 20:53:55 +0100

    Adds tables row component

---
+++ src/components/tables/TablesRow.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="items-list-row tables-row">
+    <div class="name table-name">{{table.location}} - {{table.name}}
+      <Tag :label="table.seats"/>
+    </div>
+    <div class="list-actions">
+      <button class="list-button">Edit</button>
+      <button class="list-button">Delete</button>
+    </div>
+  </div>
+</template>
+
+<script>
+import Tag from "../layout/Tag";
+export default {
+  name: "TablesRow",
+
+  props: {
+    table:{
+      required: true,
+      type: Object
+    }
+  },
+
+  data: function () {
+    return {}
+  },
+
+  created() {
+  },
+
+  components: {Tag},
+
+  computed: {},
+
+  methods: {}
+}
+</script>
