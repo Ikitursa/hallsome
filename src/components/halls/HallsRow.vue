b34de511648c111ab1aa98060dceec5f3057132f
Maja <pedic.maja@gmail.com>
2021-11-28 19:46:10 +0100

    Adds halls list

---
+++ src/components/halls/HallsRow.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="items-list-row halls-row">
+    <div class="name hall-name">{{hall.title}}
+      <div class="list-tag">{{ hall.manager }}</div>
+    </div>
+    <div class="list-actions">
+      <button class="list-button">Edit</button>
+      <button class="list-button">Delete</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HallsRow",
+
+  props: {
+    hall:{
+      required: true,
+      type: Object
+    }},
+
+  data: function () {
+    return {}
+  },
+
+  created() {
+  },
+
+  components: {},
+
+  computed: {},
+
+  methods: {}
+}
+</script>
--- src/views/Halls.vue
+++ src/views/Halls.vue
@@ -1,10 +1,55 @@
 <template>
-<h1>Halls</h1>
+  <h1>Halls</h1>
+  <div class="container halls">
+
+    <div class="card">
+
+      <div class="card-header">
+        <button class="button-rounded button-add-new">Add new</button>
+      </div>
+
+      <div class="items-list halls-list">
+        <HallsRow v-for="hall in halls" key:="hall.id" :hall="hall"/>
+      </div>
+
+    </div>
+  </div>
 </template>
 
 <script>
+import HallsRow from "../components/halls/HallsRow";
+import axios from "axios";
 export default {
-  name: "Halls"
+  name: "Halls",
+  components: {HallsRow},
+  created() {
+    this.fetchHalls()
+  },
+  data: function () {
+    return {
+      halls: null,
+    }
+
+  },
+  methods:{
+    fetchHalls() {
+      const url = 'https://hallsome-44d13-default-rtdb.europe-west1.firebasedatabase.app/halls.json'
+      axios.get(url).then(({data, status}) => {
+            const keys = Object.keys(data)
+
+            this.halls = keys.map(key => {
+
+              return {
+                ...data[key],
+                id: key
+              }
+            })
+          }
+      ).catch(error => {
+        console.log(error)
+      })
+    }
+  }
 }
 </script>
 
